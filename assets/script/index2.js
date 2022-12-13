f19c17867c428192a84ea876a016cdf705d7215e
inder <cheema.inder10@gmail.com>
2022-12-13 09:51:00 -0600

    update

--- assets/script/index2.js
+++ assets/script/index2.js
@@ -30,23 +30,14 @@ zoom: 1 // starting zoom
 });
  
 // Add geolocate control to the map.
-map.addControl(
-new mapboxgl.GeolocateControl({
-positionOptions: {
-enableHighAccuracy: true
-},
-// When active the map will receive updates to the device's location as it changes.
-trackUserLocation: true,
-// Draw an arrow next to the location dot to indicate which direction the device is heading.
-showUserHeading: true
-})
-);
-
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// Listening to the resize event
-window.addEventListener('resize', () => {
-  vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// map.addControl(
+// new mapboxgl.GeolocateControl({
+// positionOptions: {
+// enableHighAccuracy: true
+// },
+// // When active the map will receive updates to the device's location as it changes.
+// trackUserLocation: true,
+// // Draw an arrow next to the location dot to indicate which direction the device is heading.
+// showUserHeading: true
+// })
+// );
